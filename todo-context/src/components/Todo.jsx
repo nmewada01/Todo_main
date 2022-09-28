f5ad6e95aef69e62fa013bc749ad93f8c5309868
Naresh Rajput <nareshmewada014@gmail.com>
2022-09-28 23:15:14 +0530

    responsive done

--- todo-context/src/components/Todo.jsx
+++ todo-context/src/components/Todo.jsx
@@ -1,6 +1,7 @@
 import { useReducer, useState } from "react";
 import Reducer from "../Reducer/Reducer";
-
+import { RiChatDeleteFill } from "react-icons/ri";
+import { TiTick } from "react-icons/ti";
 const Todo = () => {
   const [{ todos, todoCount }, dispatch] = useReducer(Reducer, {
     todos: [],
@@ -31,27 +32,27 @@ const Todo = () => {
                 textDecoration: t.completed ? "line-through" : "",
               }}
             >
-              <div className="left">
-                <button
-                  className="button"
-                  onClick={() => dispatch({ type: "TOGGLE-TODO", idx })}
-                >
-                  o
-                </button>
-              </div>
-              <div>
-                {" "}
-                {t.text}
-                {idx}
-              </div>
-              <div className="right">
-                {" "}
-                <button
-                  className="button2"
-                  onClick={() => dispatch({ type: "DELETE-TODO", idx })}
-                >
-                  X{" "}
-                </button>
+              <div className="patti">
+                <div className="left">
+                  <button
+                    className="button"
+                    onClick={() => dispatch({ type: "TOGGLE-TODO", idx })}
+                  >
+                    <TiTick size={"25px"} />
+                  </button>
+                </div>
+                <div style={{ fontSize: "20px", textTransform: "capitalize" }}>
+                  {t.text}
+                  {idx}
+                </div>
+                <div className="right">
+                  <button
+                    className="button2"
+                    onClick={() => dispatch({ type: "DELETE-TODO", idx })}
+                  >
+                    <RiChatDeleteFill size={"25px"} />
+                  </button>
+                </div>
               </div>
             </div>
           ))}
